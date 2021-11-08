60239d048e27f8f1404511b99d4add1100d74d88
unknown <hshanto707@gmail.com>
2021-11-08 08:51:57 +0600

    initial commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDJ9v135JUy9pfddsADKNptVpZODYOoah8",
+  authDomain: "coding-club-institute-hhs.firebaseapp.com",
+  projectId: "coding-club-institute-hhs",
+  storageBucket: "coding-club-institute-hhs.appspot.com",
+  messagingSenderId: "663820975692",
+  appId: "1:663820975692:web:1e1e22cfe4430fa20d7ee9"
+};
+
+export default firebaseConfig;
